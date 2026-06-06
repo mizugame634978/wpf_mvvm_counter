46549f7293d080d6fe2d40f396ad0006af0267be
mizugame634978 <ryouta1853@gmail.com>
2026-06-06 21:49:43 +0900

    docs: Json用クラスにコメントを追加

--- MVVVM_Counter/Service/JsonCounterStorage.cs
+++ MVVVM_Counter/Service/JsonCounterStorage.cs
@@ -6,15 +6,21 @@ using System.Text.Json;
 
 namespace MVVVM_Counter.Service;
 
+/// <summary>
+/// カウンターの値をJSONファイルに保存・読み込みするクラス
+/// </summary>
 public class JsonCounterStorage
 {
+    // アプリの実行ディレクトリ（bin/Debug/...）に保存される
     private const string FilePath = "counter.json";
 
     public int Load()
     {
+        // using System.IO; を書くことで、System.IO.File を File と省略して書いている
         if(!File.Exists(FilePath)) return 0;
 
         var json = File.ReadAllText(FilePath);
+        // JSONの文字列をint型に変換する
         return JsonSerializer.Deserialize<int>(json);
     }
 
